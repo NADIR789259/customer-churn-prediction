2ae891e2a6b1ce08214622be1dc6c13d75a2e934
Nadir Khan <nadirkhan5864@gmail.com>
2026-07-18 15:33:39 +0530

    M

--- Customer-Churn-Prediction/app.py
+++ Customer-Churn-Prediction/app.py
@@ -7,7 +7,9 @@ import pickle
 
 
 def load_css():
-    with open("assets/style.css") as f:
+    css_path = Path(__file__).parent / "assets" / "style.css"
+
+    with open(css_path, "r", encoding="utf-8") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 load_css()
